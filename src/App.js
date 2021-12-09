d7f3f2fd2959034a3912339652ad81b375297e54
Christopher Maia <christopherwillians7@gmail.com>
2021-12-09 04:11:17 -0300

    projeto adicionado ao github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import './global.css';
+
+function App() {
+  return (
+    <>
+    <Header />
+      <div className="container">
+        <h1>App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
