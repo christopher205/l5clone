b87c27b49c9a6c534e8a5c9b411670a8c39d6fe4
Christopher Maia <christopherwillians7@gmail.com>
2021-12-12 18:08:52 -0300

    adicionado hooks para poder trocar a cor do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import logo from '../images/logo-l5.png'
 import iconlinkedin from '../images/linkedin-svgrepo-com.svg'
 import iconfacebook from '../images/facebook-svgrepo-com.svg'
 import iconyoutube from '../images/youtube-svgrepo-com.svg'
 import arrow_down_icon from '../images/arrow-204-64.png'
 import './style.css'
+import { AddClass, RemoveClass } from "../utils/AddandRemoveClass";
 
 function Header() {
+
+    const [scrolled, setScrolled] = useState(false)
+    useEffect(() => {
+        function handleScroll() {
+            if (window.scrollY > 15) {
+                setScrolled(true)
+                AddClass()
+
+            } else {
+                setScrolled(false)
+                RemoveClass()
+
+            }
+        }
+        window.addEventListener('scroll', handleScroll)
+    }
+    , [])
+
     return (
         <header className="header">
-            <div className="header-row">
+            <div className="header-row" value={scrolled} >
                 <span> <a href="#"> Fale com Vendas</a> </span>
                 <span> <a href="#"> Atendimento a Clientes </a></span>
             </div>
@@ -20,29 +39,28 @@ function Header() {
                 </a>
                 <ul className="navbar-links">
                     <li>
-                        <a href="#">Home</a>
+                        <a href="#" className="link">Home</a>
                     </li>
                     <li>
-                        <a href="#">Sobre</a>
+                        <a href="#" className="link" >Sobre</a>
                     </li>
                     <li>
-                        <a href="#">Clientes</a>
+                        <a href="#" className="link" >Clientes</a>
                     </li>
                     <li>
-                        <a href="#" className="nav-a" >Produtos
+                        <a href="#" className="link" >Produtos
                         </a>
                         <img className="icon-dropdown" src={arrow_down_icon} />
                     </li>
                     <li>
-                        <a href="#">Blog</a>
+                        <a href="#" className="link" >Blog</a>
                     </li>
                     <li>
-                        <a href="#">Na Midia</a>
+                        <a href="#" className="link" >Na Midia</a>
                     </li>
                     <li>
-                        <a href="#">Parceiros</a>
+                        <a href="#" className="link" >Parceiros</a>
                     </li>
-
                         <img className="icon" src={iconlinkedin} alt="icon-linkedin" />
 
                         <img className="icon" src={iconfacebook} alt="icon-facebook" />
