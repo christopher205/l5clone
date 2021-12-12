798e39ee290bc6f82159857635b4dca8143bd4dd
Christopher Maia <christopherwillians7@gmail.com>
2021-12-12 18:10:46 -0300

    criada 2 funçoes que adicionam e removem classes

--- src/utils/AddandRemoveClass.js
+++ src/utils/AddandRemoveClass.js
@@ -5,6 +5,11 @@ function AddClass() {
     .classList.add('header-row-scrolled')
     document.querySelector('.last-row')
     .classList.add('last-row-scrolled')
+    document.querySelector('.logo')
+    .classList.add('logo-scrolled')
+    document.querySelectorAll('.link').forEach(link => {
+        link.classList.add('link-scrolled')
+    })
 }
 function RemoveClass() {
     document.querySelector('.navbar')
@@ -13,6 +18,12 @@ function RemoveClass() {
     .classList.remove('header-row-scrolled')
     document.querySelector('.last-row')
     .classList.remove('last-row-scrolled')
+    document.querySelector('.logo')
+    .classList.remove('logo-scrolled')
+    document.querySelectorAll('.link').forEach(link => {
+        link.classList.remove('link-scrolled')
+    })
+    
 }
 
 module.exports = { AddClass, RemoveClass }
